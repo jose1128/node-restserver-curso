943ea5dd75b48bf7f224b2848bd0dab991513f3f
ProgramacionDistribuida <jb912618@gmail.com>
2020-07-20 19:39:42 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlBD;
 if(process.env.NODE_ENV === 'dev'){
     this.urlBD = 'mongodb://localhost:27017/cafe';
 }else{
-    this.urlBD = 'mongodb+srv://josebl97:4rtY3nSeGccY9zMa@cluster0.4oecl.mongodb.net/cafe?retryWrites=true&w=majority'
+    this.urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = this.urlBD;
